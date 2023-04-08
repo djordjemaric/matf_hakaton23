f111302dda5a8738d5190dad1b236924e1964e05
bole <bandric404@gmail.com>
2023-04-08 14:50:30 +0200

    Test

--- hakaton/src/main/java/com/example/hakaton/HelloApplication.java
+++ hakaton/src/main/java/com/example/hakaton/HelloApplication.java
@@ -15,6 +15,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        //idemo nis
     }
 
     public static void main(String[] args) {

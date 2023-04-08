f87ad9120b8fe737accd6b82c4796a305cec80fd
cvija25 <lcvijicc@gmail.com>
2023-04-08 17:20:57 +0200

    pocetni nivo

--- hakaton/src/main/java/com/example/hakaton/HelloApplication.java
+++ hakaton/src/main/java/com/example/hakaton/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.hakaton;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,13 +13,25 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        HBox root = new HBox();
 
-        //idemo nis
+        VBox left = new VBox();
+        Button nivo1 = new Button("1");
+        Button nivo2 = new Button("2");
+        Button nivo3 = new Button("3");
+
+        left.getChildren().addAll(nivo1, nivo2, nivo3);
+
+        VBox right = new VBox();
+        Button dalje = new Button("dalje");
+        right.getChildren().addAll(dalje);
+
+        root.getChildren().addAll(left, right);
+
+        Scene scena = new Scene(root,800,640);
+        stage.setScene(scena);
+        stage.setTitle("Nivoi");
+        stage.show();
     }
 
     public static void main(String[] args) {

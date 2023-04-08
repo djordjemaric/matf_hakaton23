f8a70a2208c59dba6c8999963c7015babc293b8b
unknown <draganceperic@gmail.com>
2023-04-08 18:53:09 +0200

    Main

--- hakaton/src/main/java/com/example/hakaton/HelloApplication.java
+++ hakaton/src/main/java/com/example/hakaton/HelloApplication.java
@@ -2,20 +2,25 @@ package com.example.hakaton;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WINDOW_HEIGHT = 500;
+
     @Override
     public void start(Stage stage) throws IOException {
-        HBox root = new HBox();
+        Group root = new Group();
+        HBox nivoi = new HBox();
 
-        VBox left = new VBox();
         Button nivo1 = new Button("1");
         Button nivo2 = new Button("2");
         Button nivo3 = new Button("3");
@@ -25,15 +30,12 @@ public class HelloApplication extends Application {
             tutorijal1.show();
         });
 
-        left.getChildren().addAll(nivo1, nivo2, nivo3);
-
-        VBox right = new VBox();
         Button dalje = new Button("dalje");
-        right.getChildren().addAll(dalje);
 
-        root.getChildren().addAll(left, right);
+        nivoi.getChildren().addAll(nivo1, nivo2, nivo3, dalje);
+        root.getChildren().addAll(nivoi);
 
-        Scene scena = new Scene(root,800,640);
+        Scene scena = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setScene(scena);
         stage.setTitle("Nivoi");
         stage.show();
